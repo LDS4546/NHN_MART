011fa50b74e610530e0458536ffa1f66a0c6c95b
이동수 <dongsu6666@naver.com>
2024-09-13 15:56:01 +0900

    step 8 clear

--- src/main/java/com/nhnacademy/thread/ThreadPool.java
+++ src/main/java/com/nhnacademy/thread/ThreadPool.java
@@ -35,19 +35,26 @@ public class ThreadPool {
 
     public ThreadPool(Runnable runnable){
         //TODO#8-1-1 default 생성자 구현, poolSize = DEFAULT_POOL_SIZE를 사용합니다.
-        this.poolSize=0;
-        this.runnable = null;
-        this.threadList = null;
+        this.poolSize=DEFAULT_POOL_SIZE;
+        this.runnable = runnable;
+        this.threadList = new ArrayList<>(poolSize);
     }
 
     public ThreadPool(int poolSize, Runnable runnable) {
         //TODO#8-1-2 thread pool size <0 다면 IllegalArgumentException이 발생 합니다.
-
+        if(poolSize < 0){
+            throw new IllegalArgumentException();
+        }
 
         //TODO#8-1-3 runable == null 이면 IllegalArgumentException 발생 합니다.
-
+        if(runnable == null){
+            throw new IllegalArgumentException();
+        }
 
         //TODO#8-1-4 runnable 이 Runnable의 구현체가 아니라면 IllegalArgumentException 발생 합니다.
+        if (!(runnable instanceof Runnable)) {
+            throw new IllegalArgumentException();
+        }
 
 
         //TODO#8-1-5 poolSize, runnable, threadList 초기화
@@ -59,7 +66,7 @@ public class ThreadPool {
         createThread();
     }
 
-    private void createThread(){
+    private synchronized void createThread(){
         for(int i=0; i<poolSize; i++){
             threadList.add(new Thread(runnable));
         }
@@ -72,6 +79,8 @@ public class ThreadPool {
         * */
         for(int i=0; i<poolSize; i++){
             //구현
+            Thread thread =threadList.get(i);
+            thread.start();
         }
     }
 
@@ -83,11 +92,20 @@ public class ThreadPool {
 
         for(Thread thread : threadList){
             //구현
+            if(Objects.nonNull(thread) && thread.isAlive() ){
+                thread.interrupt();
+            }
         }
 
         //TODO#8-1-8 join()를 이용해서 모든 thread가 종료될 떄 까지 대기 상태로 만듭니다.
         for(Thread thread : threadList){
             //궈현
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                log.debug("{}",e.getMessage(),e);
+            }
         }
     }
 }
--- src/test/java/com/nhnacademy/thread/util/RequestChannelTest.java
+++ src/test/java/com/nhnacademy/thread/util/RequestChannelTest.java
@@ -35,13 +35,18 @@ class RequestChannelTest {
         long queueSize = (long) readFieldValue.get();
 
         //TODO#8-2-8 기본 생성자를 이용해서 생성된 requestChannel의 queueSize가 10인지 검증 합니다.
-
+        Assertions.assertEquals(10L, queueSize);
     }
 
     @Test
     @DisplayName("queueSize=-5")
     void constructorTest2(){
         //TODO#8-2-9 RequestChannel 객체 생성시 queueSize -5 이면 IllegalArgumentException 발생하는지 검증 합니다.
+        Assertions.assertThrows(IllegalArgumentException.class, ()->{
+            RequestChannel requestChannel = new RequestChannel(-5);
+        });
+
+
 
     }
 
@@ -50,6 +55,9 @@ class RequestChannelTest {
     void addRequest_5_times() throws Exception {
         RequestChannel requestChannel = new RequestChannel();
         //TODO#8-2-10 requestChannel에 5개의 아무것도 실행하지 않는 작업을(Executable) 등록 합니다. Executable : ()->{} 사용합니다.
+        for(int i=0; i<5; i++){
+            requestChannel.addRequest(()->{});
+        }
 
 
         Try<Object> readFieldValue = ReflectionUtils.tryToReadFieldValue(RequestChannel.class, "requestQueue", requestChannel);
@@ -66,6 +74,9 @@ class RequestChannelTest {
 
         Thread thread = new Thread(()->{
             //TODO#8-2-11 requestChannel에 11개의 빈 작업을 등록하는 thread를 구현 하세요. 빈 작업: ()->{}
+            for(int i =0; i<11; i++){
+                requestChannel.addRequest(()->{});
+            };
         });
 
         thread.start();
@@ -89,6 +100,8 @@ class RequestChannelTest {
         }
         //TODO#8-2-12 requestChannel 작업을 할당 받아 실행 하세요.
 
+        Executable executable = requestChannel.getRequest();
+        executable.execute();
 
         Try<Object> readFieldValue = ReflectionUtils.tryToReadFieldValue(RequestChannel.class, "requestQueue", requestChannel);
         Queue queue = (Queue) readFieldValue.get();
@@ -114,7 +127,7 @@ class RequestChannelTest {
         log.debug("{} : {}", thread.getName(),thread.getState());
 
         //TODO#8-2-13 thread의 상태가 WAITING 상태인지 검증 합니다.
-
+        Assertions.assertEquals(Thread.State.WAITING, thread.getState());
 
         thread.interrupt();
     }
